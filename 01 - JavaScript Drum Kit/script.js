0887cee7f64899c8db1baa7f465f76e47856d3d1
AbdulAsyraf <abdul.asyrafah@gmail.com>
2022-09-20 22:10:32 +0800

    Finished the js exercise for events

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,5 +1,17 @@
-window.addEventListener('keydown', function (e){
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const selected = document.querySelector(`div[data-keys="${e.keyCode}"]`);
-    selected.classList.add('playing');
-});
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+window.addEventListener('keydown', playSound);
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
